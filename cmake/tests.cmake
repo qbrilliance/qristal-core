584d3258bbc04bdfe90fdf4dd98e87a5f21c2542
John Helm <john.helm@quantum-brilliance.com>
2022-11-16 06:39:16 +0000

    sdk ci changes

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -69,18 +69,25 @@ foreach(test ${circuit_tests})
   add_gtest(${test} PATH circuits)
 endforeach()
 
-# Upgrade conan and build standalone qiskit-aer for mock testing
-# TODO do we really want to do this?
-execute_process(COMMAND ${Python_EXECUTABLE} -m pip install conan --upgrade --quiet)
-include(ExternalProject)
-ExternalProject_Add(qiskit-aer
-  GIT_REPOSITORY    https://github.com/Qiskit/qiskit-aer.git
-  GIT_TAG           0.10.4
-  SOURCE_DIR        "${CMAKE_BINARY_DIR}/qiskit-aer-src"
-  BINARY_DIR        "${CMAKE_BINARY_DIR}/qiskit-aer-build"
-  CMAKE_ARGS    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}
-)
-set(QB_STANDALONE_AER_EXE ${CMAKE_CURRENT_BINARY_DIR}/bin/qasm_simulator)
+find_program(QB_STANDALONE_AER_EXE qasm_simulator)
+
+if(${QB_STANDALONE_AER_EXE} STREQUAL "QB_STANDALONE_AER_EXE-NOTFOUND")
+  # Upgrade conan and build standalone qiskit-aer for mock testing
+  # TODO do we really want to do this?
+  execute_process(COMMAND ${Python_EXECUTABLE} -m pip install conan --upgrade --quiet)
+  include(ExternalProject)
+  ExternalProject_Add(qiskit-aer
+    GIT_REPOSITORY    https://github.com/Qiskit/qiskit-aer.git
+    GIT_TAG           0.10.4
+    SOURCE_DIR        "${CMAKE_BINARY_DIR}/qiskit-aer-src"
+    BINARY_DIR        "${CMAKE_BINARY_DIR}/qiskit-aer-build"
+    CMAKE_ARGS    -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_CURRENT_BINARY_DIR}
+  )
+  set(QB_STANDALONE_AER_EXE ${CMAKE_CURRENT_BINARY_DIR}/bin/qasm_simulator)
+else()
+  add_custom_target(qiskit-aer)
+endif()
+
 configure_file(tests/lambda/fake_aer_server.py.in
   ${CMAKE_BINARY_DIR}/plugins/qb_lambda/tests/fake_aer_server.py
 )
