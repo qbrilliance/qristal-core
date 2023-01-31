03be32bab91ace64027e914b9914c28a88388606
Pat Scott <pat.scott@quantum-brilliance.com>
2023-01-31 21:35:41 +0000

    Fix issues with PYTHONPATH when installing as root without a virtual environment

--- cmake/py_packages_path.cmake
+++ cmake/py_packages_path.cmake
@@ -5,53 +5,65 @@
 # If the path is given directly when invoking cmake, just use the value given there.  Otherwise, work it out.
 if(NOT PYTHON_PACKAGES_PATH)
 
-  # Work out if the user is running with root privileges or not.
-  execute_process(RESULT_VARIABLE FAILURE OUTPUT_VARIABLE IAM COMMAND whoami OUTPUT_STRIP_TRAILING_WHITESPACE)
-  if(FAILURE)
-    message(FATAL_ERROR "Could not determine if user is running with root privileges or not!")
-  endif()
+  # Determine if using venv or not
+  execute_process(RESULT_VARIABLE PYTHON_VENV_USED
+                  ERROR_VARIABLE DISCARD
+                  COMMAND python3 -c "import sys; assert sys.prefix == sys.base_prefix")
+    if(FAILURE)
+      message(FATAL_ERROR "Failed to determine whether a Python virtual environment is being used or not.")
+    endif()
 
-  # Switch according to whether install is run as root or not.
-  if (IAM STREQUAL "root")
+  # Switch depending on whether running with a virtual environment or not
+  if (PYTHON_VENV_USED)
 
-    # Running as root/sudo, so we should write to the system site-packages dir.
+    # Running in a virtual environment
     execute_process(RESULT_VARIABLE FAILURE
                     OUTPUT_VARIABLE PYTHON_PACKAGES_PATH
                     OUTPUT_STRIP_TRAILING_WHITESPACE
                     COMMAND python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")
     if(FAILURE)
-      message(FATAL_ERROR "Failed to determine the path to your Python site-packages dir. Please set it manually with -DPYTHON_PACKAGES_PATH=/path/to/dir.")
+      message(FATAL_ERROR "Failed to determine the path to your Python packages dir. Please set it manually with -DPYTHON_PACKAGES_PATH=/path/to/dir.")
     endif()
-    message(STATUS "Installing with root privileges -- writing SDK package info to ${PYTHON_PACKAGES_PATH}.")
+    message(STATUS "Installing in a virtual environment -- writing SDK package info to ${PYTHON_PACKAGES_PATH}.")
 
   else()
-    # Not running with root privileges, so we should try to write to the local site-packages dir.
 
-    # Determine if using venv or not
-    execute_process(RESULT_VARIABLE PYTHON_VENV_USED
-                    ERROR_VARIABLE DISCARD
-                    COMMAND python3 -c "import sys; assert sys.prefix == sys.base_prefix")
+    # Work out if the user is running with root privileges or not.
+    execute_process(RESULT_VARIABLE FAILURE OUTPUT_VARIABLE IAM COMMAND whoami OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(FAILURE)
+      message(FATAL_ERROR "Could not determine if user is running with root privileges or not!")
+    endif()
 
-    if(PYTHON_VENV_USED)
+    # Switch according to whether install is run as root or not.
+    if (IAM STREQUAL "root")
+
+      # Running as root/sudo, so we should write to the system-wide packages dir.
       execute_process(RESULT_VARIABLE FAILURE
                       OUTPUT_VARIABLE PYTHON_PACKAGES_PATH
                       OUTPUT_STRIP_TRAILING_WHITESPACE
-                      COMMAND python3 -c "import sysconfig; print(sysconfig.get_paths()['purelib'])")
+                      COMMAND python3 -c "import site; print(site.getsitepackages()[0])")
+      if(FAILURE)
+        message(FATAL_ERROR "Attempting a root install, but failed to determine your global Python packages path.")
+      endif()
+      message(STATUS "Performing a root install -- writing SDK package info to ${PYTHON_PACKAGES_PATH}.")
+
     else()
+
+      # Not running with root privileges, so we should try to write to the local site-packages dir.
       execute_process(RESULT_VARIABLE FAILURE
                       OUTPUT_VARIABLE PYTHON_PACKAGES_PATH
                       OUTPUT_STRIP_TRAILING_WHITESPACE
                       COMMAND python3 -m site --user-site)
-    endif()
-
-    if(FAILURE)
-      if(FAILURE STREQUAL "1" OR FAILURE STREQUAL "2")
-        message(FATAL_ERROR "Attempting a local install, but failed because your local Python installation has its user site directory (site-packages) disabled. Try enabling it, or installing as root.")
-      else()
-        message(FATAL_ERROR "Attempting a local install, but failed to determine the path to your local Python site-packages directory. Please set it manually with -DPYTHON_PACKAGES_PATH=/path/to/dir.")
+      if(FAILURE)
+        if(FAILURE STREQUAL "1" OR FAILURE STREQUAL "2")
+          message(FATAL_ERROR "Attempting a local install, but failed because your local Python installation has its user site directory (site-packages) disabled. Try enabling it, or installing as root.")
+        else()
+          message(FATAL_ERROR "Attempting a local install, but failed to determine the path to your local Python site-packages directory. Please set it manually with -DPYTHON_PACKAGES_PATH=/path/to/dir.")
+        endif()
       endif()
+      message(STATUS "Performing a local install -- writing SDK package info to ${PYTHON_PACKAGES_PATH}.")
+
     endif()
-    message(STATUS "Performing a local install -- writing SDK package info to ${PYTHON_PACKAGES_PATH}.")
 
   endif()
 
